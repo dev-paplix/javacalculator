edfcc2cab0920310629c74fc851e7e1c3c917dea
devops.arman <paplix.dev@gmail.com>
2024-11-06 22:40:13 +0800

    Adding the copycalc and Factory method

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,9 @@ public class Calculator {
     private int version;
 
     public Calculator(){
-        System.out.println("Defualt Calculator.");
+        this.model = "Basic Model";
+        this.version = 1;
+        System.out.println("Calculator created: model =  " + model + " " + "Version =  " + version);
     }
 
     public Calculator(String model){
@@ -24,10 +26,37 @@ public class Calculator {
         System.out.println("Calculator created: model =  " + model + " " + "Version =  " + version);
     }
 
+    public Calculator(Calculator other){
+        this.model = other.model;
+        this.version = other.version;
+        System.out.println("Calculator created: model =  " + model + " " + "Version =  " + version);
+    }
+
+    public static Calculator createSientifCalculator(int version){
+        Calculator calc = new Calculator("Scientific", version);
+        System.out.println("Calculator created: model =  " + calc.model + " " + "Version =  " + calc.version);
+        return calc;
+    }
+
+    public void displayInfo(){
+        System.out.println("Calculator created: model =  " + model + " " + "Version =  " + version);
+    }
+
     public static void main(String[] args){
         Calculator calc1 = new Calculator();
         Calculator calc01 = new Calculator("Graphing", 2);
         Calculator calc2  = new Calculator("Scientific");
+        Calculator calc3 = new Calculator(4);
         Calculator calc4 = new Calculator(3);
+        Calculator calcCopy = new Calculator(calc01);
+
+        Calculator sientificCalc = Calculator.createSientifCalculator(3);
+        calc01.displayInfo();
+        calc1.displayInfo();
+        calc2.displayInfo();
+        calc3.displayInfo();
+        calc4.displayInfo();
+        calcCopy.displayInfo();
+        sientificCalc.displayInfo();
     }
 }
